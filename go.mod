ba2c36a7e5adfdbb0e401af23e8d89c253f2cfba
imattf <imattf@pm.me>
2020-06-09 18:59:24 -0700

    add sayanthing

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/imattf/sayit
+
+go 1.13
---
+++ sayanything.go
@@ -0,0 +1,18 @@
+//before making code change do...
+// go mod init github.com/imattf/sayit
+//now...
+// build the code
+// git add -A
+// git commit -m "my msg"
+// git tag v0.1.0
+// git push
+// git push --tags
+
+package sayit
+
+//Favs is a ...
+func Favs() []string {
+	xs := []string{"one", "two", "three"}
+	return xs
+
+}
